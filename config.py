52de0a27fbf7d8788f1425e16c7a0b92474208b4
jarry <jarry@prnd.co.kr>
2023-12-14 00:04:30 +0900

    [modify] ConfigManager - create config directory

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from dataclasses import asdict
 
@@ -6,13 +7,21 @@ from dto.config import Config
 
 
 class ConfigManager:
-    file_path = '~/.fosslight/config.json'
+    file_path = os.path.expanduser('~/.fosslight/config.json')
 
-    def save_config(self, config: Config):
-        with open(self.file_path, 'w') as f:
+    @classmethod
+    def save_config(cls, config: Config):
+        dir_path = os.path.dirname(cls.file_path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        with open(cls.file_path, 'w') as f:
             json.dump(asdict(config), f, indent=4)
 
-    def read_config(self) -> Config:
-        with open(self.file_path, 'r') as f:
-            config = json.load(f)
-            return Config(**config)
+    @classmethod
+    def read_config(cls) -> Config:
+        try:
+            with open(cls.file_path, 'r') as f:
+                config = json.load(f)
+                return Config(**config)
+        except FileNotFoundError:
+            return Config()
