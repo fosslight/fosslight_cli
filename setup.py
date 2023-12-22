4d96ba01c109c232a3e8067e1b1b88f9ff8d012a
JEOO2327 <1gkswogns@gmail.com>
2023-12-22 13:20:20 +0900

    [Improve] setup - add license
    Signed-off-by: JEOO2327 <1gkswogns@gmail.com>

--- setup.py
+++ setup.py
@@ -5,15 +5,26 @@ with open('requirements.txt', 'r') as f:
     required = f.read().splitlines()
 
 
-setup(
-    name='fosslight_cli',
-    version='0.0.4',
-    packages=find_packages(),
-    install_requires=required,
-    python_requires=">=3.8",
-    entry_points={
-        'console_scripts': [
-            "fosslight-cli = src.__main__:main",
-        ]
-    }
-)
+if __name__ == '__main__':
+    setup(
+        name='fosslight_cli',
+        version='0.0.4',
+        packages=find_packages(),
+        install_requires=required,
+        python_requires=">=3.8",
+        entry_points={
+            'console_scripts': [
+                "fosslight-cli = src.__main__:main",
+            ]
+        },
+        license='Apache-2.0',
+        url='https://github.com/fosslight/fosslight_cli',
+        download_url='https://github.com/fosslight/fosslight_cli',
+        classifiers=[
+            'License :: OSI Approved :: Apache Software License',
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+        ],
+    )
